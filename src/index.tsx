fcb70a6118200aec7bb41b96d4ee3a2b4fc2c1f5
Sosuke Suzuki <aosukeke@gmail.com>
2019-08-04 18:16:21 +0900

    Modify to change title according to filepath

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import FileContext from "./lib/FileContext";
 import Main from "./components/Main";
 
 const App: React.FC = () => {
   const [filepath, setFilepath] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (filepath == null) {
+      document.title = "Dumbo"
+    } else {
+      document.title = filepath;
+    }
+  }, [filepath])
+
   return (
     <FileContext.Provider value={{ filepath, setFilepath }}>
       <Main />
