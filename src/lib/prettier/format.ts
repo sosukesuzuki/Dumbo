342ba073b0e7423015455cc7a0b72f1a6fa4d21a
Sosuke Suzuki <aosukeke@gmail.com>
2019-08-04 23:43:25 +0900

    Remove prettier

--- src/lib/prettier/format.ts
+++
@@ -1,9 +0,0 @@
-import prettier from "prettier/standalone";
-import markdown from "prettier/parser-markdown";
-
-export default function(text: string): string {
-  return prettier.format(text, {
-    parser: "markdown",
-    plugins: [markdown]
-  });
-}
