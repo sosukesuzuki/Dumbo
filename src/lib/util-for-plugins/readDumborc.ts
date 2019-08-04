b56e8c8b89f2a6c8cdf56d73f81a22058ee4daa3
Sosuke Suzuki <aosukeke@gmail.com>
2019-08-05 00:06:43 +0900

    Implement readDumborc

---
+++ src/lib/util-for-plugins/readDumborc.ts
@@ -0,0 +1,47 @@
+import { join } from "path";
+import readFile from "../fs/readFile";
+import stat from "../fs/stat";
+
+type Dumborc = {
+  plugins: string[];
+};
+
+export default async function(): Promise<Dumborc> {
+  const homeDir =
+    process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
+
+  if (!homeDir) {
+    return {
+      plugins: []
+    };
+  }
+
+  const dumborcpath = join(homeDir, "/.dumbo", "/dumborc.json");
+
+  let isExists: boolean = false;
+  try {
+    await stat(dumborcpath);
+    isExists = true;
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      isExists = false;
+    }
+  }
+
+  if (isExists) {
+    const value = await readFile(dumborcpath, "utf8");
+    const dumborc = JSON.parse(value);
+
+    if (
+      dumborc["plugins"] &&
+      Array.isArray(dumborc["plugins"]) &&
+      Object.keys(dumborc).length === 1
+    ) {
+      return dumborc as Dumborc;
+    }
+  }
+
+  return {
+    plugins: []
+  };
+}
