2013b58c9076a4565277bdc417a7ace1fbed1fd5
Sosuke Suzuki <aosukeke@gmail.com>
2019-08-10 01:04:49 +0900

    Implement LoadingElephant in root

--- src/components/atoms/LoadingElephant.tsx
+++ src/components/atoms/LoadingElephant.tsx
@@ -1,21 +1,8 @@
 import React from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: block;
-  animation: spin 1.5s linear infinite;;
-  @keyframes spin {
-    0% {transform: rotate(0deg);}
-    100% {transform: rotate(360deg);}
-  }
-  font-size: 200px;
-  width: 200px;
-  margin: auto;
-`;
 
 const LoadingElephant: React.FC = () => {
   return (
-    <Container>🐘</Container>
+    <div className="loading">🐘</div>
   )
 }
 
